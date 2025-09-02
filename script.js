032d2947cf5cabbd2d9637d6c6b651ff4bca0706
RakeshKumar-2007 <dhonirakeshkumar07@gmail.com>
2025-09-02 20:16:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// Smooth scroll for navbar links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth"
+    });
+  });
+});
+
+// Console log
+window.onload = function() {
+  console.log("Portfolio loaded successfully!");
+};
